45af87a532fb0eecaba73d0a6504f9d9c5807c22
FIRA Air <fira.air.sim@outlook.com>
2022-08-06 03:43:58 +0430

    Fixed main league scene
    Changed the color of the gate frame to magenta

--- fira_challenge_env/script/run.sh
+++ fira_challenge_env/script/run.sh
@@ -4,4 +4,15 @@ killall -9 roslaunch
 killall -9 rosout
 killall -9 gzserver
 killall -9 gzclient
-roslaunch fira_challenge_env main.launch
+#!/bin/sh
+
+#find where the 'sjtu_drone' is
+pack_path=$(rospack find sjtu_drone)
+
+#export the gazebo pathes
+export GAZEBO_MODEL_PATH=$pack_path/models:$GAZEBO_MODEL_PATH
+export GAZEBO_RESOURCE_PATH=$pack_path:/usr/share/gazebo-11:/usr/share/gazebo-11:/usr/share/gazebo_models:$GAZEBO_RESOURCE_PATH
+export GAZEBO_PLUGIN_PATH=$pack_path/plugins:$GAZEBO_PLUGIN_PATH
+
+#call the client of Gazebo
+roslaunch fira_challenge_env main.launch 
